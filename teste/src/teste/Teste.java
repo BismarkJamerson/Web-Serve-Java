ad4aa5499bccaeb536e448b4954cc90c6fe8f134
Bismark Jamerson da Silva <57026193+BismarkJamerson@users.noreply.github.com>
2020-11-29 18:55:46 -0300

    atualização

---
+++ teste/src/teste/Teste.java
@@ -0,0 +1,18 @@
+
+package teste;
+
+
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int ii = 0 ;
+    while (ii < 2) {
+            		ii++;
+            		System.out.println("ii = " + ii);
+            	}
+    
+}
+}
